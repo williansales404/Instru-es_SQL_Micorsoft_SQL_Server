550f6bdde5ddf00a5bfb1aad88c1c2029c27b1e6
williansales404 <willian9542@gmail.com>
2026-07-20 08:11:36 -0300

    sequences

---
+++ 21. [SQL Server] Sequences/2- Sequences.sql
@@ -0,0 +1,81 @@
+-- V. SEQUENCES
+
+-- O que é?
+-- Uma sequência (Sequence) é um objeto que utilizamos para criação de números 
+-- sequenciais automáticos. São usados especialmente para gerar valores sequenciais
+-- únicos para as chaves primárias das tabelas.
+
+-- Dessa forma, não precisamos ficar preenchendo a sequência de ids manualmente (como fizemos
+-- até então), podemos gerar automaticamente por meio de uma sequence.
+
+/* Sintaxe
+*
+
+CREATE SEQUENCE nome_sequencia
+AS tipo
+START WITH n
+INCREMENT BY n
+MAXVALUE n | NO MAXVALUE
+MINVALUE n | NO MINVALUE
+CYCLE | NO CYCLE;       -- quando atinge o valor máximo, pode ou não voltar do começo
+
+*/
+
+
+
+-- Crie uma sequência para o id_cliente
+
+CREATE SEQUENCE clientes_seq
+AS INT
+START WITH 1
+INCREMENT BY 1
+MAXVALUE 9999999
+NO CYCLE
+
+
+-- Próximo valor da sequência
+
+SELECT NEXT VALUE FOR clientes_seq
+
+
+-- Excluir uma sequence
+DROP SEQUENCE clientes_seq
+
+SELECT name, start_value, increment 
+FROM sys.sequences;
+
+SELECT TABLE_NAME 
+FROM INFORMATION_SCHEMA.TABLES;
+
+
+
+
+-- Usando a sequence na prática
+
+-- Crie uma sequência para o id_projeto
+
+CREATE SEQUENCE projetos_seq
+AS INT
+START WITH 1
+INCREMENT BY 1
+NO MAXVALUE
+NO CYCLE
+
+
+CREATE TABLE dProjeto(
+	id_projeto INT,
+	nome_projeto VARCHAR(100) NOT NULL,
+	CONSTRAINT dareas_id_area_pk PRIMARY KEY(id_projeto)
+)
+
+
+INSERT INTO dProjeto(id_projeto, nome_projeto) VALUES
+	(NEXT VALUE FOR projetos_seq, 'Planejamento Estratégico'),
+	(NEXT VALUE FOR projetos_seq, 'Desenvolvimento de App'),
+	(NEXT VALUE FOR projetos_seq, 'Plano de Negócios'),
+	(NEXT VALUE FOR projetos_seq, 'Visualização 3D')
+
+INSERT INTO dProjeto(id_projeto, nome_projeto) VALUES
+	(NEXT VALUE FOR projetos_seq, 'Mapeamento de Processos')
+
+SELECT * FROM dProjeto
---
+++ 21. [SQL Server] Sequences/exercicios_sequences.sql
@@ -0,0 +1,73 @@
+create sequence cliente_seq
+as int
+start with 1
+increment by 1
+no maxvalue
+no cycle
+
+create sequence carro_seq
+as int
+start with 1
+increment by 1
+no maxvalue
+no cycle
+
+create sequence locacoes_seq
+as int
+start with 1
+increment by 1
+no maxvalue
+no cycle
+
+create table DimCliente(
+	id_cliente int default next value for cliente_seq,
+	nome_cliente varchar(250) not null,
+	cnh varchar(100) not null,
+	cartao varchar(100) not null,
+	constraint DimCliente_id_cliente_pk primary key(id_cliente),
+	constraint DimCliente_cnh_un unique(cnh)
+)
+
+create table DimCarro(
+	id_carro int default next value for carro_seq,
+	placa varchar(100) not null,
+	modelo varchar(250) not null,
+	tipo varchar(250) not null,
+	constraint DimCarro_id_carro_pk primary key(id_carro),
+	constraint DimCarro_modelo_ck check(tipo in('Hatch', 'Sedan', 'SUV')),
+	constraint DimCarro_placa_un unique(placa)
+)
+
+create table FactLocacoes(
+	id_locacao int default next value for locacoes_seq,
+	data_locacao date default getdate(),
+	data_devolucao date default getdate(),
+	id_carro int,
+	id_cliente  int,
+	constraint FactLocacoes_id_locacao_pk primary key(id_locacao),
+	constraint FactLocacoes_id_carro_fk foreign key(id_carro) references DimCarro(id_carro),
+	constraint FactLocacoes_id_cliente_fk foreign key(id_cliente) references DimCliente(id_cliente)
+)
+
+select * from DimCliente
+select * from DimCarro
+select * from FactLocacoes
+
+insert into DimCliente(nome_cliente, cnh, cartao)
+values
+	('Blue Silva', '12345678901', '4539 1488 0343 6467'),
+	('Ana Costa', '98765432109', '5500 1234 5678 9012'),
+	('Carlos Mendes', '45678912345', '6011 4321 8765 2109')
+
+INSERT INTO DimCarro (placa, modelo, tipo)
+VALUES
+    ('JBL3C40', 'Polo','Hatch'),
+    ('FRT9X21', 'Golf', 'Sedan'),
+    ('PLM7K88', 'Civic', 'SUV');
+
+
+INSERT INTO FactLocacoes (data_locacao, data_devolucao, id_carro, id_cliente)
+VALUES
+    ('2026-01-01', '2026-01-30', 3, 7),  -- Civic alugado pelo Carlos Mendes
+    ('2026-02-15', '2026-02-16', 4, 5);  -- Golf alugado pelo Blue Silva
+
